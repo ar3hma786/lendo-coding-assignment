efd3610e43012ac3fc68f3a45ea9e8f3f7515d55
ar3hma786 <arahmanhussain786@gmail.com>
2024-05-25 20:48:08 +0400

    made changes in model

--- lendo/src/main/java/com/lendoassignment/lendo/model/Posts.java
+++ lendo/src/main/java/com/lendoassignment/lendo/model/Posts.java
@@ -19,7 +19,7 @@ import lombok.NoArgsConstructor;
 public class Posts {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long post_id;
     
     @ManyToOne
